df0fba6141f4e5aff868c836be7cd399a506986d
unknown <paolomiguel108@gmail.com>
2023-02-28 22:23:59 +0800

    ReactJS-functionRedux-CRUD

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { addUser, deleteUser, updateUser } from './features/Users';
 
 function App() {
+  const userList = useSelector((state) => state.users.value);
+  const dispatch = useDispatch();
+  const [person, setperson] = React.useState({ name: '', username: '' });
+  const [newUserName, setNewUserName] = React.useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setperson({ ...person, [name]: value });
+    console.log(person);
+  };
+
+  const handleSubmit = () => {
+    const newPerson = { ...person, id: userList[userList.length - 1].id + 1 };
+    console.log(newPerson);
+    dispatch(addUser(newPerson));
+    setperson({ name: '', username: '' });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='addUsers'>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name...'
+          onChange={handleChange}
+          value={person.name}
+        />
+        <input
+          type='text'
+          name='username'
+          placeholder='Username...'
+          onChange={handleChange}
+          value={person.username}
+        />
+        <button onClick={handleSubmit}>Add User</button>
+      </div>
+      <div className='displayUsers'>
+        {userList.map((user) => (
+          <div key={user.id}>
+            <h1>{user.name}</h1>
+            <h2>{user.username}</h2>
+            <input
+              type='text'
+              name='newusername'
+              placeholder='New Username...'
+              onChange={(e) => setNewUserName(e.target.value)}
+            />
+            <button onClick={() => dispatch(updateUser({ id: user.id, username: newUserName }))}>
+              Update
+            </button>
+            <button onClick={() => dispatch(deleteUser(user.id))}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
